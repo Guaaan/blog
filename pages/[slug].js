8ed8a3b0e63041891d174e8d81fd7f427d42f525
John Doe <juampablo.txt@gmail.com>
2021-12-06 19:27:09 -0300

    mensaje

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,27 +1,27 @@
 import { getFileBySlug, getFiles } from "../lib/mdx";
 
 export default function Post({ source, frontmatter }) {
-    console.log(source);  
-    return <h1>Post</h1>;
- }
+  console.log(source);
+  return <h1>Post</h1>;
+}
 
- export async function getStaticProps({ params }) {
-     const { source, frontmatter  } = await getFileBySlug(params. slug);
+export async function getStaticProps({ params }) {
+  const { source, frontmatter } = await getFileBySlug(params.slug);
 
-     return {
-         props: { props, frontmatter }, 
-     };
- }
+  return {
+    props: { source, frontmatter },
+  };
+}
 
- export async function getStaticPaths() {
-      const posts = await getFiles();
-      const paths = posts.map((post) => ({
-      params: { 
-            slug: post.replace (".mdx", ""),
-        },
-        }));
-        return {
-            paths,
-            fallback: false, 
-        }
- }
+export async function getStaticPaths() {
+  const posts = await getFiles();
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.replace(/\.mdx/, ""),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
